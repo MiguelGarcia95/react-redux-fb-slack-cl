2db864b0c5f1a0eb140049070a96a421d7862329
Miguel <mgarcia95951@gmail.com>
2018-12-21 20:38:41 -0600

    fixed firebase issue

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,10 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
+const API_KEY = process.env.FIREBASE_API_KEY;
+
 var config = {
-    apiKey: "AIzaSyBSp8IUedTYKmt6o6RmKpZEdmDIjeQFMcc",
+    apiKey: API_KEY,
     authDomain: "react-slack-clone-8ca74.firebaseapp.com",
     databaseURL: "https://react-slack-clone-8ca74.firebaseio.com",
     projectId: "react-slack-clone-8ca74",
