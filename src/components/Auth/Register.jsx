c87843103c189c033eaf12dff64ae6bf6333986c
Miguel <mgarcia95951@gmail.com>
2018-12-25 13:54:36 -0600

    created register page and functionality

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
-
+import { Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+import firebase from '../../firebase';
 class Register extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  }
+
+  handleChange = (e) => this.setState({[e.target.name]: e.target.value});
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    firebase.auth()
+            .createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(createdUser => {
+              console.log(createdUser);
+            }).catch(err => {
+              console.error(err);
+            })
+
+  }
+
   render () {
+    const {username, email, password, passwordConfirmation} = this.state;
     return(
-      <div>Register</div>
+      <Grid textAlign='center' verticalAlign='middle' className='app'>
+        <Grid.Column style={{maxWidth:450}}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange' />
+            Register for DevChat
+          </Header>
+          <Form size='large' onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input fluid name='username' icon='user'iconPosition='left' placeholder='Username' onChange={this.handleChange} type='text' value={username} />
+              <Form.Input fluid name='email' icon='mail'iconPosition='left' placeholder='Email Address' onChange={this.handleChange} type='email' value={email}/>
+              <Form.Input fluid name='password' icon='lock'iconPosition='left' placeholder='Password' onChange={this.handleChange} type='password' value={password}/>
+              <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left' placeholder='Confirm Password' onChange={this.handleChange} type='password' value={passwordConfirmation}/>
+              <Button color='orange' fluid size='large'>Submit</Button>
+            </Segment>
+          </Form>
+          <Message>Already a user? <Link to='/login'>Login</Link></Message>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+// import 'firebase/firestore';
 import 'firebase/database';
 import 'firebase/storage';
-
-const API_KEY = process.env.FIREBASE_API_KEY;
+import {FIREBASE_API_KEY} from './config/apiKeys.js';
 
 var config = {
-    apiKey: API_KEY,
+    apiKey: FIREBASE_API_KEY,
     authDomain: "react-slack-clone-8ca74.firebaseapp.com",
     databaseURL: "https://react-slack-clone-8ca74.firebaseio.com",
     projectId: "react-slack-clone-8ca74",
