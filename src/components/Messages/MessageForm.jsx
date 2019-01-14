0d072401e5d34bdb63e2c66b55089aa1260acb7d
Miguel <mgarcia95951@gmail.com>
2019-01-14 15:43:32 -0600

    adding file upload modal

--- src/components/Messages/MessageForm.jsx
+++ src/components/Messages/MessageForm.jsx
@@ -3,6 +3,7 @@ import uuidv4 from 'uuid/v4';
 import {Segment, Button, Input} from 'semantic-ui-react';
 import firebase from '../../firebase';
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
@@ -116,7 +117,7 @@ class MessageForm extends React.Component {
   }
 
   render () {
-    const {errors, message, loading, modal} = this.state;
+    const {errors, message, loading, modal, uploadState, percentUploaded} = this.state;
     return (
       <Segment className='message__form'>
         <Input
@@ -152,6 +153,10 @@ class MessageForm extends React.Component {
             modal={modal}
             closeModal={this.closeModal}
             uploadFile={this.uploadFile}
+          />
+          <ProgressBar 
+            uploadState={uploadState}
+            percentUploaded={percentUploaded}
           />
         </Button.Group>
       </Segment>
