08f0e9e8ed606f2055724191fddb8d6b694287f5
Miguel <mgarcia95951@gmail.com>
2019-01-15 13:15:14 -0600

    get most active user

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Segment, Comment} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import {setUserPosts} from '../../actions';
 import MessageHeader from './MessageHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
@@ -45,6 +47,7 @@ class Messages extends React.Component {
         messagesLoading: false
       })
       this.countUniqueUsers(loadedMessages)
+      this.countUserPosts(loadedMessages);
     });
   }
 
@@ -98,6 +101,21 @@ class Messages extends React.Component {
     const plural = uniqueUsers.length > 1 ? 'users' : 'user';
     const numUniqueUsers = `${uniqueUsers.length} ${plural}`;
     this.setState({numUniqueUsers: numUniqueUsers})
+  }
+  
+  countUserPosts = messages => {
+    let userPosts = messages.reduce((acc, message) => {
+      if (message.user.name in acc) {
+        acc[message.user.name].count += 1;
+      } else {
+        acc[message.user.name] = {
+          avatar: message.user.avatar,
+          count: 1
+        }
+      }
+      return acc;
+    }, {});
+    this.props.setUserPosts(userPosts);
   }
 
   displayMessages = messages => {
@@ -182,4 +200,4 @@ class Messages extends React.Component {
   }
 }
 
-export default Messages;
+export default connect(null, {setUserPosts})(Messages);
