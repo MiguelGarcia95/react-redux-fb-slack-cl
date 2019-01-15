37a4ddeae9d0e3e007e226572b827175319ee203
Miguel <mgarcia95951@gmail.com>
2019-01-14 20:29:04 -0600

    add dms

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -15,7 +15,7 @@ class SidePanel extends React.Component {
       >
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
-        <DirectMessages />
+        <DirectMessages currentUser={currentUser} />
       </Menu>
     )
   }
