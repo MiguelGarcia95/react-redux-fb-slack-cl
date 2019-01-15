adc13d1bd89aeabd8c35430a629bf7ab97db3c11
Miguel <mgarcia95951@gmail.com>
2019-01-15 12:29:12 -0600

    create meta panel

--- src/components/MetaPanel/MetaPanel.jsx
+++ src/components/MetaPanel/MetaPanel.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import {Segment, Accordion, Header, Icon} from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
+  state = {
+    activeIndex: 0
+  }
   render () {
+    const {activeIndex} = this.state;
     return (
-      <div>MetaPanel</div>
+      <Segment>
+        <Header as='h3' attached='top'>
+          About # Channel
+        </Header>
+        <Accordion styled attached='true'>
+          <Accordion.Title active={activeIndex === 0} index={0}>
+
+          </Accordion.Title>
+        </Accordion>
+      </Segment>
     )
   }
 }
