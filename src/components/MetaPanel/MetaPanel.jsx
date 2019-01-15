4c661ce403e24895cd072821350edfdcf0794462
Miguel <mgarcia95951@gmail.com>
2019-01-15 12:35:48 -0600

    create meta panel

--- src/components/MetaPanel/MetaPanel.jsx
+++ src/components/MetaPanel/MetaPanel.jsx
@@ -5,6 +5,14 @@ class MetaPanel extends React.Component {
   state = {
     activeIndex: 0
   }
+
+  setActiveIndex = (e, titleProps) => {
+    const {index} = titleProps;
+    const {activeIndex} = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({activeIndex: newIndex});
+  }
+
   render () {
     const {activeIndex} = this.state;
     return (
@@ -13,9 +21,44 @@ class MetaPanel extends React.Component {
           About # Channel
         </Header>
         <Accordion styled attached='true'>
-          <Accordion.Title active={activeIndex === 0} index={0}>
-
+          <Accordion.Title 
+            active={activeIndex === 0} 
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='info' />
+            Channel Details
           </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0} >
+            Details
+          </Accordion.Content>
+
+          <Accordion.Title 
+            active={activeIndex === 1} 
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='user circle' />
+            Top Posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1} >
+            Posters
+          </Accordion.Content>
+
+          <Accordion.Title 
+            active={activeIndex === 2} 
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown' />
+            <Icon name='pencil alternate' />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2} >
+            Creator
+          </Accordion.Content>
         </Accordion>
       </Segment>
     )
